31d132fe50ef938056665c62a03f66c7b2a20ebf
Takuma <i.taku0415@gmail.com>
2020-09-19 02:50:03 +0900

    [add] header

---
+++ components/organisms/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div class="header">
+      <LogoLink></LogoLink>
+    </div>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import LogoLink from '@/components/molecules/LogoLink';
+export default defineComponent({
+  components: {
+    LogoLink,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/variables.scss';
+header {
+  background-color: $primary;
+  padding: 1rem;
+  width: 100%;
+}
+
+.header {
+  display: inline-flex;
+}
+</style>
