6c0293d85d9276c7722e334af4a759a971c72049
Takuma <i.taku0415@gmail.com>
2020-09-19 02:49:11 +0900

    [update] Logo.vue

---
+++ components/atoms/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="@/assets/logo.png" :width="width" alt="ロゴ" />
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+export default defineComponent({
+  props: {
+    width: {
+      type: String,
+      required: false,
+    },
+  },
+});
+</script>
+<style lang="scss"></style>
