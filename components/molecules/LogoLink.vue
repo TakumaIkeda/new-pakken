076cee28c81be5acbabb342a9a0791f2c0c62a77
Takuma <i.taku0415@gmail.com>
2020-09-19 02:49:51 +0900

    [add] LogoLink.vue

---
+++ components/molecules/LogoLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <nuxt-link to="/">
+    <Logo width="100rem"></Logo>
+  </nuxt-link>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import Logo from '@/components/atoms/Logo';
+export default defineComponent({
+  components: {
+    Logo,
+  },
+});
+</script>
+
+<style></style>
